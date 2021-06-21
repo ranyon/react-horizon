9456e89568526fd22b4e0ac235c2aae986a16e91
Mark Mensah <kwakumensahj@gmail.com>
2021-06-21 17:33:22 +0100

    rendered offices

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './navbar';
 import Carousel from './carousel';
+import Offices from './Offices';
 
 function App() {
   return (
    <>
    <Navbar/>
    <Carousel/>
+   <Offices/>
 
    </>
   );
