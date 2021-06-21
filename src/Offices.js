b5c484339e3ed11af2123d933a80ac07224c87da
Mark Mensah <kwakumensahj@gmail.com>
2021-06-21 15:15:19 +0100

    setting up offices component

---
+++ src/Offices.js
@@ -0,0 +1,57 @@
+import React from 'react'
+
+const Offices = () => {
+    return (
+      <div>
+        <div className="container">
+        <div className="row col-grid">
+            <h1 className="text-center">PLANS</h1>
+            <br/>
+            <br/>
+            <div className=" col-md-3">
+              <div className="thumbnail">
+                <img src={} alt="offices"/>
+                <div className="caption">
+                  <h3>Virtual offices</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, animi.</p>
+                  
+                </div>
+              </div>
+            </div>
+            <div className=" col-md-3">
+              <div className="thumbnail">
+                <img src={} alt="offices"/>
+                <div className="caption">
+                  <h3>Virtual offices</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, animi.</p>
+                  
+                </div>
+              </div>
+            </div>
+            <div className=" col-md-3">
+              <div className="thumbnail">
+                <img src={} alt="offices"/>
+                <div className="caption">
+                  <h3>Virtual offices</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, animi.</p>
+                  
+                </div>
+              </div>
+            </div>
+            <div className=" col-md-3">
+              <div className="thumbnail">
+                <img src={} alt="offices"/>
+                <div className="caption">
+                  <h3>Virtual offices</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, animi.</p>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+    </div>
+      </div>
+    )
+}
+
+export default Offices ;
