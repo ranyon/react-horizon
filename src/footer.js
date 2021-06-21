98c4f29973675092b9c2f593db6820bbfd7b639f
Mark Mensah <kwakumensahj@gmail.com>
2021-06-21 18:16:52 +0100

    added footer component

---
+++ src/footer.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+             <footer>
+        <div class="container">
+            <div class="row">
+                <br/>
+                
+               
+                <div class="col-md-4 f-col">
+                    <h4 class="text-center">VISIT US IN GHANA</h4>
+                    <h5 class="text-center">Accra,Ghana</h5>
+                    
+                    <p class="text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit, dolor?</p>
+                </div>
+                <div class="col-md-4 f-col">
+                    <h4 class="text-center">VISIT US IN GHANA</h4>
+                    <h5 class="text-center">Accra,Ghana</h5>
+                    
+                    <p class="text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit, dolor?</p>
+                </div>
+                <div class="col-md-4 f-col">
+                    <h4 class="text-center">VISIT US IN GHANA</h4>
+                    <h5 class="text-center">Accra,Ghana</h5>
+                    
+                    <p class="text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit, dolor?</p>
+                </div>
+               
+               
+               
+            </div>
+        </div>
+        <br/>
+    </footer>
+        </div>
+    )
+}
+
+export default Footer
